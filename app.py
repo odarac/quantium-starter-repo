b7101d0b74c400d2021f28c6362afd531bb1d923
odarac <odarachan151@gmail.com>
2025-08-25 21:09:45 +1000

    Add filtering by region

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+from dash.dependencies import Input, Output
 
 app = Dash()
 
@@ -17,13 +18,47 @@ app.layout = html.Div(children=[
     html.H1(children='Sales Amount of Pink Morsels with time'),
 
     dcc.Graph(
-        id='sales_graph',
+        id='sales-graph',
         figure=fig
+    ),
+
+    dcc.RadioItems(
+        id='region-options',
+        options=[
+            {'label': 'North', 'value': 'north'},
+            {'label': 'South', 'value': 'south'},
+            {'label': 'East', 'value': 'east'},
+            {'label': 'West', 'value': 'west'},
+            {'label': 'All', 'value': 'all'}
+        ],
+        value='all',  # Initial selected value
+        inline=True
     )
 ])
 
-if __name__ == '__main__':
+@app.callback(
+    Output('sales-graph', 'figure'),
+    Input('region-options', 'value')
+)
+def update_region(region):
+    if region != 'all':
+        filtered_df = df[df.Region == region]
+        fig = px.line(filtered_df, x="Date", y="Sales", labels= {
+            "Date": "Time",
+            "Sales": "Sales Amount",
+        })
+    else:
+        fig = px.line(df, x="Date", y="Sales", labels= {
+            "Date": "Time",
+            "Sales": "Sales Amount",
+        })
+    return fig
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+
 
 
 
